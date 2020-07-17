8b288118c164f04aec1eccbc8191d76ca1d7a239
Maxime Chardon <maxime.chardon@epita.fr>
2020-07-17 19:45:45 +0200

    add main.py the main script

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#Author: Popey
+
+################################################################################
+#                                   Import                                     #
+################################################################################
+
+# Packages
+import nmap3 #For Scan Port/Ip
+import whois #For the localisation of domain name
+import dns.resolver #DNS to resolve domain name
+
+# 
